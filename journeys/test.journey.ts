8bc22340027d94084a179947a0f240538a236fb8
root <aloa@tobeit.es>
2022-07-26 16:59:14 +0200

    fourth commit

---
+++ journeys/test.journey.ts
@@ -0,0 +1,6 @@
+import { journey, step, expect } from '@elastic/synthetics';
+
+journey('Webpage Load', ({ page, params }) => {
+  step('launch app', async () => {
+    await page.goto('https://www.elpais.com);
+  });
